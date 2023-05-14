86bfbd3b212d102aca8730132621f501157e1eed
Adepetu Alex <alexadepetu@gmail.com>
2023-05-14 01:30:29 +0100

    commit

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -12,34 +12,56 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  List<IconData> _iconList = [Icons.home, Icons.chat, Icons.map];
+
   @override
   Widget build(BuildContext context) {
+    List<widget> _navBarItemList = [];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text("title"),
+        title: Text(widget.title),
       ),
       bottomNavigationBar: Row(
         children: [
-          buildNavigationItem(Icons.home),
-          buildNavigationItem(Icons.chat),
-          buildNavigationItem(Icons.map),
+          buildNavigationItem(Icons.home, 0),
+          buildNavigationItem(Icons.chat, 1),
+          buildNavigationItem(Icons.map, 2),
         ],
       ),
     );
   }
 
-  Container buildNavigationItem([IconData? icon]) {
-    return Container(
-      child: Icon(icon),
-      height: 70,
-      width: MediaQuery.of(context).size.width / 3,
-      decoration: BoxDecoration(
-          // color: Colors.blue,
-          gradient: LinearGradient(colors: [
-        Colors.blue.withOpacity(0.5),
-        Colors.blue.withOpacity(0.03),
-      ], begin: Alignment.bottomCenter, end: Alignment.topCenter)),
+  Widget buildNavigationItem(IconData icon, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container(
+        child: Icon(
+          icon,
+          color: index == _selectedIndex ? Colors.blue : Colors.grey,
+        ),
+        height: 70,
+        width: MediaQuery.of(context).size.width / 3,
+        decoration: index == _selectedIndex
+            ? BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(width: 5, color: Colors.blue)),
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.blue.withOpacity(0.5),
+                    Colors.blue.withOpacity(0.03),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              )
+            : null,
+      ),
     );
   }
 }
