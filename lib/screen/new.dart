9207a0b98f7d24df711f05732db15e6b68d2a182
Adepetu Alex <alexadepetu@gmail.com>
2023-05-18 11:34:35 +0100

    commit

--- lib/screen/new.dart
+++ lib/screen/new.dart
@@ -4,6 +4,7 @@ import 'package:bar_app/widget/lastorder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class homePage extends StatefulWidget {
   const homePage({super.key});
@@ -63,65 +64,73 @@ class _homePageState extends State<homePage> {
                 SizedBox(
                   height: 20,
                 ),
-                Container(
-                  width: double.infinity,
-                  height: 200,
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 216, 68, 242),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'ROTARI BRUT',
-                                style: TextStyle(
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                'Wine Type: Rose Brut',
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.white),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                'Quantity: 1.5 L',
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.white),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Container(
-                                alignment: Alignment.center,
-                                height: 40,
-                                width: 110,
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(20)),
-                                child: Text(
-                                  '£ 12.0',
+                GestureDetector(
+                  onDoubleTap: () {
+            //          Navigator.push(
+            //   context,
+            //   MaterialPageRoute(builder: (context) => ()),
+            // );
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 216, 68, 242),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'ROTARI BRUT',
                                   style: TextStyle(
+                                      fontSize: 25,
                                       fontWeight: FontWeight.bold,
-                                      fontSize: 15),
+                                      color: Colors.white),
                                 ),
-                              ),
-                            ],
-                          ),
-                          Image.asset('images/5.png')
-                        ],
-                      )),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'Wine Type: Rose Brut',
+                                  style: TextStyle(
+                                      fontSize: 15, color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'Quantity: 1.5 L',
+                                  style: TextStyle(
+                                      fontSize: 15, color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  height: 40,
+                                  width: 110,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: Text(
+                                    '£ 12.0',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Image.asset('images/5.png')
+                          ],
+                        )),
+                  ),
                 ),
                 SizedBox(
                   height: 20,
@@ -163,8 +172,39 @@ class _homePageState extends State<homePage> {
                 ),
               ],
             ),
-          )
+          ),
         ],
+      ),
+      bottomNavigationBar: Container(
+        // color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: GNav(
+              backgroundColor: Colors.purple,
+              color: Colors.white,
+              activeColor: Colors.white,
+              tabBackgroundColor: Colors.grey.shade800,
+              gap: 8,
+              tabBorderRadius: 0,
+              onTabChange: (index) {
+                print(index);
+              },
+              padding: EdgeInsets.all(16),
+              tabs: [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Icons.map,
+                  text: 'Map',
+                ),
+                GButton(
+                  icon: Icons.chat,
+                  text: 'Chat',
+                )
+              ]),
+        ),
       ),
     );
   }
---
+++ lib/widget/navbar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../screen/new.dart';
+
+class NavigationBar extends StatefulWidget {
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    homePage(),
+    homePage(),
+    homePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
